63d0dbd41955637f533c5ea25367d8a37613b6ee
Vince Mutolo <vlmutolo@me.com>
2018-05-27 00:58:58 -0400

    changes

--- mcint/test.py
+++ mcint/test.py
@@ -6,24 +6,39 @@ from importlib import reload
 import MCIntegrator
 reload(MCIntegrator)
 
-@np.vectorize
-def f(x):
-	return x**2
 
-def g(x, y):
-	return x**2 + y**2
 
-start = time()
-runs = 10
 
-mci = MCIntegrator.MCSimpleInt(f, [[0,2]])
-mci.add_evals(runs)
-mci_res  = mci.calculate_estimates()
-end0 = time()
+runs_domain = np.linspace(1, 1000, 10, dtype=np.int)
+args_domain = np.linspace(1,   10,  3, dtype=np.int)
+time_lists = []
 
-mci = MCIntegrator.MCSimpleInt(g, [[0,2], [0,2]])
-mci.add_evals(runs)
-mci_res  = mci.calculate_estimates()
-end1 = time()
+for i in range(1, 5):
 
-print((end1 - start) / (end0 - start))
+	times = []
+
+	# Define a function to take however many arguments,
+	# square them, and then return the sum.
+	def f(*args):
+		res = 0
+		for j in range(len(args)):
+			res += args[j]**2
+		return res
+
+	# We will set the bounds to (0,1) for each
+	# variable of integration.
+	bounds = [[0,1]]*i
+
+	for num_runs in runs_domain:
+
+		start = time()
+		mci = MCIntegrator.MCSimpleInt(f, bounds)
+		mci.add_evals(num_runs)
+		mci_res  = mci.calculate_estimates()
+		end = time()
+
+		times.append(end - start)
+
+	time_lists.append(times)
+
+
